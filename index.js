53868860a7ed7401d4888278d6c732c5efdad7f9
김경호 <kyeongho94@gmail.com>
2025-05-14 19:15:45 +0900

    햄버거 수정

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
     menu.classList.toggle('active');
   });
 });
+
+document.getElementById('toggleButton').addEventListener('click', function () {
+  const menu = document.querySelector('.mb-menu');
+  if (menu.style.display === 'none' || menu.style.display === '') {
+    menu.style.display = 'block';
+  } else {
+    menu.style.display = 'none';
+  }
+});
